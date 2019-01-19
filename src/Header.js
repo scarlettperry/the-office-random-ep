96196b94c20b2f20691ce9ad81f9912504f6acb2
scarlettperry <si.perry15@gmail.com>
2019-01-19 13:17:37 -0500

    added header image

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import headerImage from './images/header_image.png'
+
+const Header = () => {
+  return (
+    <div>
+      <img src={headerImage} alt="the office"/>
+    </div>
+  )
+}
+
+export default Header
