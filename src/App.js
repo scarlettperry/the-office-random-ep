1842caa7c7e193eb44b3bb0c6f069ce0c0b1fb0f
scarlettperry <si.perry15@gmail.com>
2019-02-17 10:22:49 -0500

    random episode is revealed

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header.js'
 import Button from './Button.js'
-import Episode from './Episode.js'
-
+import EpisodeReveal from './EpisodeReveal.js'
 
 class App extends Component {
   constructor(){
     super()
     this.state={
-      office_ep: ["Threat Level Midnight", "Casino Night", "Beach Day", "Moraccan Christmas", "Diversity Training", "Local Ad", "Weight Loss Training: Part 1", "The Merger"]
+      office_ep: ["Threat Level Midnight", "Casino Night", "Beach Day", "Moraccan Christmas", "Diversity Training", "Local Ad", "Weight Loss Training: Part 1", "The Merger"],
+      episode: ""
     }
   }
 
-  randomEpClick = (e) => {
+  randomEpClick = () => {
     let episodeCollection = this.state["office_ep"]
-    console.log(episodeCollection[Math.floor(Math.random() * episodeCollection.length)])
+    this.setState({
+      episode: episodeCollection[Math.floor(Math.random() * episodeCollection.length)]
+    })
   }
 
   render() {
@@ -25,10 +27,12 @@ class App extends Component {
       <Button
         randomEpClick={this.randomEpClick}
       />
-      <Episode/>
+      <EpisodeReveal episode={this.state.episode}/>
       </div>
     );
   }
+
+
 }
 
 export default App;
---
+++ src/EpisodeReveal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EpisodeReveal = (props) => {
+  return (
+    <div>
+      <p>{props.episode}</p>
+    </div>
+  )
+}
+
+export default EpisodeReveal
